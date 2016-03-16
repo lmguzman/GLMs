aaa130d846a17f2e6af90839d3a93c2c574b38f5
lmguzman <guzman@zoology.ubc.ca>
2016-03-16 09:14:17 -0700

    Added form and challenge 2

--- ReadForm.R
+++ ReadForm.R
@@ -9,7 +9,7 @@ ClassData<-gs_title('Class Data (Responses)')
 
 #Separate the coordinates, Bromeliad number and site
 
-Data <- gs_read(ClassData, ws='Form Responses 1', range='A1:F14', col_names=TRUE)
+Data <- gs_read(ClassData, ws='Form Responses 1', range='A1:F22', col_names=TRUE)
 
 write.csv(Data, 'ClassData.csv')
 
